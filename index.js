fcc7e403b1bf8993287a5d55052d4d89804e8800
tree02179 <153999365+tree02179@users.noreply.github.com>
2025-09-19 18:27:56 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var userName = document.getElementById("tree");            
+    document.getElementById("button").addEventListener('click', () => {
+        var input = userName.value;
+        alert(input);
+    });
+        
+});
+
+
+    
+    
